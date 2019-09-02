6c1881f475a8d6b35fe4cc974124f815bd4ddbef
Issei.M <issei.m7@gmail.com>
2019-09-02 22:36:49 +0900

    Fix test

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+
   context 'in scope excluding id=1' do
-    let(:categories) { Category.where.not(id: 1).scoping { Category.all } }
-    context 'the size of categories' do
-      subject { categories.size }
-      it { is_expected.to eq 5 }
+
+    context 'with the all categories' do
+      let(:categories) { Category.where.not(id: 1).scoping { Category.all } }
+      context 'the size of categories' do
+        subject { categories.size }
+        it { is_expected.to eq 5 }
+      end
     end
+
+    context 'with the all categories' do
+      let(:parent) { Category.where.not(id: 1).scoping { Category.find_by(parent_id: 1).parent } }
+      subject { parent }
+      it { is_expected.to be_present }
+    end
+
   end
+
 end
